3e9c696991f816c4007ac15b1bd67fb0eaf0fc13
bassemdahmeni <bassem.dahmani@ensi-uma.tn>
2025-10-06 20:08:39 +0100

    main and predict test

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,5 +2,5 @@ import subprocess
 
 def test_main_runs():
     """Ensure main.py runs without error."""
-    result = subprocess.run(["python", "main.py"], capture_output=True)
+    result = subprocess.run(["python", "src\cnnClassifier\pipeline\stage_01_data_ingestion.py"], capture_output=True)
     assert result.returncode == 0, f"main.py failed with: {result.stderr.decode()}"
--- tests/test_predict.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-def test_predict_runs():
-    """Ensure predict.py runs successfully."""
-    result = subprocess.run(
-        ["python", "src/cnnClassifier/pipeline/predict.py"], capture_output=True
-    )
-    assert result.returncode == 0, f"predict.py failed with: {result.stderr.decode()}"
